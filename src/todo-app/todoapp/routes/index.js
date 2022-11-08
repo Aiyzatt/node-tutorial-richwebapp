0f35a954df7e65385ec388f80b1b605b523e6ff5
Aiyzatt <a.sassa92@gmail.com>
2022-11-08 19:17:37 +0900

    feat: knexでクエリビルド

--- src/todo-app/todoapp/routes/index.js
+++ src/todo-app/todoapp/routes/index.js
@@ -1,48 +1,46 @@
 const express = require('express');
 const router = express.Router();
 const mysql = require('mysql');
-
-const connection = mysql.createConnection({
-  host: 'db', // docker-compose.ymlで定義
-  user: 'root',
-  password: 'password',
-  database: 'todo_app',
-  stringifyObjects: true, // Objectによるsqlインジェクションの防止
-});
+const knex = require('../db/knex');
+const knexfile = require('../knexfile');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  const user_id = 1;
-  const sql = 'select * from tasks where user_id = ?';
-  const placeholder =  [user_id];
-  connection.query(sql, placeholder, (error, results) => {
-    console.log(error);
-    console.log(results);
-    res.render('index', {
-      title: 'Todo App',
-      todos: results,
+  const user_id = 2;
+  knex('tasks').select('*').where('user_id', user_id)
+    .then((results) => {
+      console.log(results);
+      res.render('index', {
+        title: 'Todo App',
+        todos: results,
+        user_id: user_id,
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+      res.render('index', {
+        title: 'Todo App',
+      });
     });
-  });
 });
 
 /* POST home page. */
 router.post('/', function(req, res, next) {
-  connection.connect((err) => {
-    if (err) {
-      console.log('error connecting: ' + err.stack);
-      return
-    }
-    console.log('success');
-  });
-
-  const user_id = 1;
+  const user_id = req.body.user_id;
   const todo = req.body.todo;
-  const sql = 'insert into tasks (user_id, content) values (?, ?)';
-  const placeholder = [user_id, todo];
-  connection.query(sql, placeholder, (error, results) => {
-    console.log(error);
-    res.redirect('/');
-  });
+  knex('tasks').insert({
+    user_id: user_id,
+    content: todo,
+  })
+    .then(() => {
+      res.redirect('/');
+    })
+    .catch((error) => {
+      console.log(error);
+      res.render('index', {
+        title: 'Todo App',
+      });
+    });
 });
 
 module.exports = router;
