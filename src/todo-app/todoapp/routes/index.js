e0a231209db1e32d121c5bdb7117d2984276a230
Aiyzatt <a.sassa92@gmail.com>
2022-11-08 20:18:06 +0900

    refactor: ページタイトルを変数化、クエリビルダインデント修正

--- src/todo-app/todoapp/routes/index.js
+++ src/todo-app/todoapp/routes/index.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const knex = require('../db/knex');
+const title = 'Todo App';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   const user_id = 2;
-  knex('tasks').select('*').where('user_id', user_id)
+  knex('tasks')
+    .select('*')
+    .where('user_id', user_id)
     .then((results) => {
       console.log(results);
       res.render('index', {
-        title: 'Todo App',
+        title: title,
         todos: results,
         user_id: user_id,
       });
@@ -17,7 +20,7 @@ router.get('/', function(req, res, next) {
     .catch((error) => {
       console.log(error);
       res.render('index', {
-        title: 'Todo App',
+        title: title,
       });
     });
 });
@@ -26,17 +29,18 @@ router.get('/', function(req, res, next) {
 router.post('/', function(req, res, next) {
   const user_id = req.body.user_id;
   const todo = req.body.todo;
-  knex('tasks').insert({
-    user_id: user_id,
-    content: todo,
-  })
+  knex('tasks')
+    .insert({
+      user_id: user_id,
+      content: todo,
+    })
     .then(() => {
       res.redirect('/');
     })
     .catch((error) => {
       console.log(error);
       res.render('index', {
-        title: 'Todo App',
+        title: title,
       });
     });
 });
